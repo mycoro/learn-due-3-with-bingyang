c1a64d33db43e9c7edc2b33aa605e8e86e959a71
mycoro <mayracoronilla9@gmail.com>
2025-03-09 13:59:24 -0500

    pass data to child components with props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,76 @@
 <template>
-  <div>Hello, Vue!</div>
+  <div style="background-color: orange; padding: 5px;">
+  <p>This is the App component</p>
+  <h2> {{ blogPostId }} - {{ blogPostTitle }}</h2>
+  <h4>{{ blogPostContent }}</h4>
+  <button @click="blogPostTitle = 'A different title'">Change blog post title</button>
+  <BlogPost id="1" blogPostTitle="What is a muggle?" blogPostContent="A muggle is a person who likes any sort of magical ability..."></BlogPost>
+  <BlogPost id="1" blog-post-title="What is a muggle?" blog-post-content="A muggle is a person who likes any sort of magical ability..."></BlogPost>
+
+  <hr />
+  <BlogPost
+    :id="blogPostId"
+    :blog-post-title="blogPostTitle"
+    :blog-post-content="blogPostContent"
+  ></BlogPost>
+
+  <hr />
+
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    :id="post.id"
+    :blog-post-title="post.blogPostTitle"
+    :blog-post-content="post.blogPostContent"
+  ></BlogPost>
+
+  <hr />
+
+<BlogPost v-for="post in posts" :key="post.id" v-bind="post"></BlogPost>
+</div>
 </template>
 
 <script setup>
+import BlogPost from './BlogPost.vue'
+import { ref } from 'vue'
+
+let blogPostId = ref(2)
+let blogPostTitle = ref('HP and the Cursed Child Broadway production suspended until April')
+
+let blogPostContent = ref(
+  'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+)
+
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  }
+])
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="blog-post">
+        <p>
+            {{ message }}
+        </p>
+        <!-- display the id, title, and content of a blog post-->
+         <h2> {{ id }} - {{ blogPostTitle }}</h2>
+         <h4>{{ blogPostContent }}</h4>
+         <button @click="changeBlogPostTitle">Change blog post title</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let message = ref('This is the BlogPost component.')
+
+const props = defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+console.log(props.blogPostTitle)
+// defineProps({
+//     id: Number,
+//     blogPostTitle: String,
+//     blogPostContent: String
+// })
+
+function changeBlogPostTitle() {
+    props.blogPostTitle = 'A different title'
+}
+</script>
+
+<style scoped>
+.blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
