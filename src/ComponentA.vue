8682cddab31b336bf7d2148bca5a71ff94d9edab
mycoro <mayracoronilla9@gmail.com>
2025-03-09 15:44:38 -0500

    pass data to descendant components with provide and inject

---
+++ src/ComponentA.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>This is component A</h2>
+    <div style="background-color: lightgreen; padding-left: 10px;">
+        <ComponentB></ComponentB>
+        <ComponentB></ComponentB>
+    </div>
+</template>
+
+<script setup>
+import ComponentB from './ComponentB.vue';
+
+</script>
+
+<style scoped>
+
+</style>
